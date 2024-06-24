55a2f7eb75e8da4da41b01cf2e3500ca32a7a442
ICEILark <amonovshohjaxon9blue@gmail.com>
2024-06-24 21:43:29 +0500

    market

--- script.js
+++ script.js
@@ -1,4 +1,115 @@
-//Homework
+// const cars = [
+//     {
+//         id: 1,
+//         model: "malibu",
+//         price: 34000,
+//         year: 2023
+//     },
+//     {
+//         id: 2,
+//         model: "Onix",
+//         price: 14000,
+//         year: 2024
+//     },
+//     {
+//         id: 3,
+//         model: "Monza",
+//         price: 17000,
+//         year: 2023
+//     },
+//     {
+//         id: 4,
+//         model: "Gentra",
+//         price: 13500,
+//         year: 2022
+//     },
+//     {
+//         id: 5,
+//         model: "Captiva",
+//         price: 26000,
+//         year: 2024
+//     },
+//     {
+//         id: 6,
+//         model: "Equinox",
+//         price: 32000,
+//         year: 2024
+//     },
+// ]
+
+// const from = prompt('от этой суммы')
+// const up = prompt('до этой суммы')
+
+// let findedCars = []
+
+// cars.forEach((item) => {
+//     if(cars.price > from && cars.price < up) {
+//         findedCars.push(item)
+//     } 
+// })
+
+// console.log(findedCars);
+
+//MARKET
+
+// let milkProducts = {
+//     total: 0,
+//     total_price: 0
+// }
+// let fruitProducts = {
+//     total: 0,
+//     total_price: 0
+// }
+// let alcoProducts = {
+//     total: 0,   
+//     total_price: 0
+// }
+
+// market.filter((item)  => {
+//     if(item.type === 'milk') {
+//         milkProducts.total++
+//     } else if(item.type === 'fruit') {
+//         fruitProducts.total++
+//     } else if(item.type === 'alcohol') {
+//         alcoProducts.total++
+//     } 
+// } )
+
+// market.forEach(item => {
+//     if (item.type === 'milk') {
+//       milkProducts.total_price += item.price;
+//     } else if (item.type === 'fruit') {
+//         fruitProducts.total_price += item.price;
+//     } else if (item.type === 'alcohol') {
+//         alcoProducts.total_price += item.price;
+//     }
+//   });
+
+//   console.log(milkProducts, fruitProducts, alcoProducts);
+
+  
+//   let totalAmountOfProducts = milkProducts.total + fruitProducts.total + alcoProducts.total
+//   let totalPriceOfProducts = milkProducts.total_price + fruitProducts.total_price + alcoProducts.total_price
+  
+//   let average_price = totalPriceOfProducts / totalAmountOfProducts
+  
+// console.log(average_price);
+
+
+// let expiredProduct = []
+
+// market.forEach(item => {
+
+//   let expDate = new Date(item.expDate);
+
+//   if (new Date() > expDate) {
+//     expiredProduct.push(item);
+//   }
+// })
+
+// console.log(expiredProduct);
+
+
 let market = [
     {
         title: "moloko",
@@ -44,60 +155,51 @@ let market = [
     },
 ]
 
-let milkProducts = {
-    total: 0,
-    total_price: 0
-}
-let fruitProducts = {
-    total: 0,
-    total_price: 0
-}
-let alcoProducts = {
-    total: 0,   
-    total_price: 0
-}
+const from = prompt('от этой суммы')
+const up =prompt('до этой суммы')
 
-market.filter((item)  => {
-    if(item.type === 'milk') {
-        milkProducts.total++
-    } else if(item.type === 'fruit') {
-        fruitProducts.total++
-    } else if(item.type === 'alcohol') {
-        alcoProducts.total++
-    } 
-} )
+let findedProducts = ""
 
 market.forEach(item => {
-    if (item.type === 'milk') {
-      milkProducts.total_price += item.price;
-    } else if (item.type === 'fruit') {
-        fruitProducts.total_price += item.price;
-    } else if (item.type === 'alcohol') {
-        alcoProducts.total_price += item.price;
+    if(item.price > from && item.price < up) {
+        findedProducts += ` ${item.title + " - $" + item.price}`
     }
-  });
-
-  console.log(milkProducts, fruitProducts, alcoProducts);
-
-  
-  let totalAmountOfProducts = milkProducts.total + fruitProducts.total + alcoProducts.total
-  let totalPriceOfProducts = milkProducts.total_price + fruitProducts.total_price + alcoProducts.total_price
-  
-  let average_price = totalPriceOfProducts / totalAmountOfProducts
-  
-console.log(average_price);
-
-
-
-let expiredProduct = []
-
-market.forEach(item => {
-
-  let expDate = new Date(item.expDate);
-
-  if (new Date() > expDate) {
-    expiredProduct.push(item);
-  }
 })
 
-console.log(expiredProduct);
+if(findedProducts.trim().length > 0) {
+    const selecting = prompt(`По вашему запросу найдено: 
+        ${findedProducts} 
+
+        Если вам понравилося товар из этого списка то напишите название ниже
+        `).trim()
+}
+
+if (findedProducts.includes(selecting.toLowerCase())) {
+    const finded = market.find(item => item.title.toLowerCase() === selected.toLowerCase());
+  
+    const goingToBuy = confirm(`
+  
+  Характеристики товаров:
+  
+  Type: ${finded.title},
+  
+  Price: ${finded.price},
+  
+  ExpDate: ${finded.expDate}
+  
+  Вы хотите купить этот товар?
+  
+  `);
+  
+    if (goingToBuy === true) {
+      const purchase = +prompt(`Введите сумму продукта: ${finded.price}`);
+  
+      if (Number(purchase) >= finded.price) {
+        alert(`Поздравляем вы приобрели товар ${finded.type} ваш остаток: ${purchase - finded.price}`);
+      } else {
+        alert(`Недостаточно средств: ${finded.type}`);
+      }
+    } else {
+      alert('По вашему запросу ничего не найдено');
+    }
+  } 
